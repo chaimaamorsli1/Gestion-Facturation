d1aa7ece572e58f6373205566489ccee432799fb
chaimaa morsli <117817198+chaimaa61@users.noreply.github.com>
2024-09-20 11:01:28 +0100

    first commit

---
+++ src/main/java/com/example/gestionFacturation/GestionFacturationApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionFacturation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFacturationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFacturationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestionFacturation/GestionFacturationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionFacturation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFacturationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
