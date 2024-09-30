5f8c2ae1a9dfb37f76c15a1c0ac98035e6dc60e6
mainpush <backup592@outlook.com>
2024-09-30 15:48:04 +0100

    o

--- src/main/java/com/example/gestionFacturation/Repositories/ActeAchatRepository.java
+++ src/main/java/com/example/gestionFacturation/Repositories/ActeAchatRepository.java
@@ -1,7 +1,5 @@
 package com.example.gestionFacturation.Repositories;
 
-import com.example.gestionFacturation.entities.ActeAchat;
-import com.example.gestionFacturation.entities.Facture;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/example/gestionFacturation/Repositories/FactureRepository.java
+++ src/main/java/com/example/gestionFacturation/Repositories/FactureRepository.java
@@ -1,11 +1,9 @@
 package com.example.gestionFacturation.Repositories;
 
-import com.example.gestionFacturation.entities.Facture;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface FactureRepository extends JpaRepository<Facture, Long> {
